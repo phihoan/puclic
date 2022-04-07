a8d2cd41b864cf7a89e8d67f3d26ca2363d3b45e
govap1084 <rulesof951@gmail.com>
2022-04-07 11:39:52 +0700

    test_2

--- Validator.js
+++ Validator.js
@@ -1,14 +1,33 @@
 // * Đối tượng Validator
 function Validator (options) {
     var formElement = document.querySelector(options.form);
-    console.log(formElement);
+    if (formElement) {
+        options.rules.forEach(function (rule) {
+            var inputElement = formElement.querySelector(rule.selector);
+            var errorElement = inputElement.parentElement.querySelector('.form-message');
+
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    var errorMessage = rule.test(inputElement.value);
+                    
+                    if (errorMessage) {
+                        errorElement.innerText = errorMessage;
+                        inputElement.parentElement.classList.add('invalid');
+                    } else {
+                        errorElement.innerText = '';
+                        inputElement.parentElement.classList.remove('invalid');
+                    }
+                } 
+            }
+        })
+    }
 }
 // * Định nghĩa các rules 
 Validator.isRequired = function (selector) {
     return {
         selector: selector,
-        test: function () {
-            
+        test: function (value) {
+            return value.trim() ? undefined : 'Vui lòng nhập lại'
         }
     };
 } 
